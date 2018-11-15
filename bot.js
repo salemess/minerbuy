bb108eaf2f9039ed9b389b6243f522acb5679894
salemess <44666097+salemess@users.noreply.github.com>
2018-11-15 19:41:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('message', message => {
 
   let args = message.content.split(" ").slice(1);
 
-  if (command == "13say") {
+  if (command == "1say") {
 if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
    message.channel.sendMessage(args.join("  "))
    message.delete()
